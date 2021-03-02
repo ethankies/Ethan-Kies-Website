c11e1c2154f3bb946b25a97f2d61a4ce44b3c8b4
Ethan Kies <59584532+ethankies@users.noreply.github.com>
2021-03-02 11:54:33 -0800

    added rock paper scissors html and js

--- sketch.js
+++ sketch.js
@@ -1,187 +1,158 @@
-let numBalls; //starting population
-let spring = 0.02; //bounciness
-let gravity = 0.0;
-let friction = -1; //wall bounciness
-let balls = [];
-let healThreshold = 0;//3.5; //number of seconds before healing
 
-function randomInteger(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-var slider; // pop slider
-var spreadVect; 
+function preload(){
+  rImg = loadImage('rock.png');
+  pImg = loadImage('paper.jpg');
+  sImg = loadImage('scissors.png');
 
-function setup() {
-  createCanvas(640, 640);
-
-
-  slider = createSlider(50, 500, 250); //min max value step
-  //spreadVect = createSlider(1,10,1)
-
-  slider.style('background', 'red');
-  //spreadVect.style('background', 'red');
-  
-  numBalls = slider.value();
-
-  //button = createButton('Reset');
-  //button.mousePressed(resetSim);
-
-  textSize('32')
-  text('Choose population size', 1200, 550)
-  fill(0, 102, 153, 51);
- 
-  slider.position(window.width * 2,300);
-  //button.position(1200, 400);
-
-  for (let i = 0; i < numBalls; i++) {
-    balls[i] = new Ball(
-      random(width),
-      random(height),
-      random(10,10),
-      i,
-      balls
-    );
-   noStroke(); //no outline
-  // console.log(balls[i].col)
-  }
+ // soundFormat('mp3');
+  snip = loadSound('snip.mp3');
+  thump = loadSound('thump.mp3');
+  crinkle = loadSound('crinkle.mp3');
 }
 
-function myInputEvent(){
-  inp.input(myInputEvent);
+let hands = [];
+let diameter = 20;
+let speed = 0.02
+let speeds = [speed,-speed];
+
+function setup(){
+
+    createCanvas(400,400)
+
+    //instance
+    for(let i = 0; i < 10; i++){
+        hands[i] = new Hand('Rock',i,random(width),random(height),random(0,360));
+    }
+    for(let i = 10; i < 20; i++){
+        hands[i] = new Hand('Paper',i,random(width),random(height),random(0,360));
+    }
+    for(let i = 20; i < 30; i++){
+        hands[i] = new Hand('Scissors',i,random(width),random(height),random(0,360));
+    }
+
+    for (let i = 0; i < hands.length; i++){
+        console.log(hands[i]);
+      }
+    
 }
 
-function resetSim(){
-  newArr = [];
-  balls = [];
-  numBalls = slider.value();
-  for (let i = 0; i < numBalls; i++) {
-    balls[i] = new Ball(
-      random(width),
-      random(height),
-      random(10, 10),
-      i,
-      balls
-    );
-   noStroke(); //no outline
-  // console.log(balls[i].col)
-  }
-}
+function draw(){
+    background(175)
+    //Hand();
+    for (let i = 0; i < hands.length; i++){
 
-function draw() {
-  background(50);
-  balls.forEach(ball => {
-    ball.collide();
-    ball.move();
-    ball.doImmuneSystem();
-    ball.display();
-  });
+        if(hands[i].type == 'Rock'){
+            
+            image(rImg, hands[i].x, hands[i].y, diameter,diameter)
+
+        } else if(hands[i].type == 'Paper'){
+
+            image(pImg, hands[i].x, hands[i].y, diameter, diameter)
+
+        } else if(hands[i].type == 'Scissors'){
+
+            image(sImg, hands[i].x, hands[i].y, diameter, diameter)
+        }
+
+
+        checkCollision(hands[i].x,hands[i].y,hands[i].id,hands[i].type);
+        move(hands[i],hands[i].type,hands[i].id)
+     }
 
 }
 
-function mousePressed(){
-  var whichBall;
- var closest = balls[0];
-   for(var i = 0; i < balls.length; i++){
-    if(dist(mouseX, mouseY, balls[i].x, balls[i].y)<dist(mouseX, mouseY, closest.x, closest.y)){
-        closest = balls[i];
-    } //on click, finds the closest dot
-   }
-   
-  closest.clicked(whichBall);
-  closest = balls[0];
-}
-
-class Ball {
-  constructor(xin, yin, din, idin, oin) {
+function Hand(type,id,xin,yin,dir){
+    this.type = type;
+    this.id = id;
     this.x = xin;
     this.y = yin;
-    this.vx = randomInteger(-0.5,0.5);
-    this.vy = randomInteger(-0.5,0.5);
-    this.diameter = din;
-    this.id = idin;
-    this.others = oin;
-    this.health = "healthy";// = "healthy"
-    this.col = color(255, 204);
-  }
-  infect(){
-    this.health = "infected";
-    this.col = 'red';
-    //console.log("Ball #" + this.id + " was pressed");
-  }
-  recover(){
-    this.health = "recover";
-    this.col = color(255,104);
-  }
-  collide() {
-    for (let i = 0; i < balls.length; i++) { //this.id + 1
-      if(balls[i] == this){
-      continue;
-      }
-      let dx = balls[i].x - this.x;
-      let dy = balls[i].y - this.y;
-      let distance = sqrt(dx * dx + dy * dy);
-      let minDist = balls[i].diameter / 2 + this.diameter / 2;
-      if (distance < minDist) {
-       let angle = atan2(dy, dx);
-        let targetX = this.x + cos(angle) * minDist;
-        let targetY = this.y + sin(angle) * minDist;
-        let ax = (targetX - balls[i].x) * spring;
-        let ay = (targetY - balls[i].y) * spring;
-        this.vx -= ax;
-        this.vy -= ay;
-        balls[i].vx += ax;
-        balls[i].vy += ay;
-        
-        if(balls[i].health == "infected" && this.health == "healthy"){
-          this.infect();
-          this.healWait = randomInteger(1,60)
-          
+    this.dir = dir;
+
+    this.vx = random(speeds);
+    this.vy = random(speeds);
+ }
+
+ function checkCollision(aX,aY,id,type){
+    
+    for(let i = 0; i < hands.length; i++){
+        if(hands[i].id == id){
+            continue; //dont collide with itself
         }
-      }
-  }
-}
-
-clicked(ball){
-  let d = dist(mouseX, mouseY, this.x, this.y);
-  console.log( ball);
-      if (d < 15){
-        this.infect();
-        this.healWait = randomInteger(1,60)
-        this.doImmuneSystem();
-        //this.recover();
+        if(Math.abs(hands[i].x - aX) < diameter && Math.abs(hands[i].y - aY) < diameter){
+           // console.log(id + ' collided with ' + hands[i].id)
+            if(type == 'Rock' && hands[i].type == 'Paper'){
+            hands[id].type = 'Paper';
+            crinkle.play();
+           }
+            if(type == 'Paper' && hands[i].type == 'Scissors'){
+            hands[id].type = 'Scissors';
+            snip.play()
+            }
+            if(type == 'Scissors' && hands[i].type == 'Rock'){
+            hands[id].type = 'Rock';
+            thump.play()
+            }
+        }
     }
-}
-  move() {
-    this.vy += gravity;
-    this.x += this.vx;
-    this.y += this.vy;
-    if (this.x + this.diameter / 2 > width) {
-      this.x = width - this.diameter / 2;
-      this.vx *= friction;
-    } else if (this.x - this.diameter / 2 < 0) {
-      this.x = this.diameter / 2;
-      this.vx *= friction;
+ }
+
+function move(hand,type,id){
+    //move x and y
+    for(let i = 0; i < hands.length; i++){
+        if(hands[i].id == id){
+            continue; 
+        }
+
+        //this scis
+        if(hands[i].type == 'Scissors' && hands[id].type == 'Paper' && hands[i].x - hands[id].x < 25){
+            hands[i].x += speed;
+        } else if(hands[i].type == 'Scissors' &&hands[id].type == 'Paper' && hands[i].x - hands[id].x > 25){
+            hands[i].x -= speed;
+        } else if(hands[i].type == 'Scissors' &&hands[id].type == 'Paper' && hands[i].y - hands[id].y < 25){
+            hands[i].y += speed;
+        } else if(hands[i].type == 'Scissors' &&hands[id].type == 'Paper' && hands[i].y - hands[id].y > 25){
+            hands[i].y -= speed;
+        } else {
+
+        //this paper
+        if(hands[i].type == 'Paper' &&hands[id].type == 'Rock' && hands[i].x - hands[id].x <25){
+            hands[i].x += speed;
+        } else if(hands[i].type == 'Paper' &&hands[id].type == 'Rock' && hands[i].x - hands[id].x > 25){
+            hands[i].x -=speed;
+        } else
+        if(hands[i].type == 'Paper' &&hands[id].type == 'Rock' && hands[i].x - hands[id].y < 25){
+            hands[i].y + speed;
+        } else if(hands[i].type == 'Paper' &&hands[id].type == 'Rock' && hands[i].x - hands[id].y > 25){
+            hands[i].y + speed;
+        } else {
+        //this rock
+        if(hands[i].type == 'Rock' &&hands[id].type == 'Scissors' && hands[i].x - hands[id].x < 25){
+            hands[i].x += speed;
+        } else if(hands[i].type == 'Rock' &&hands[id].type == 'Scissors' && hands[i].x - hands[id].x > 25){
+            hands[i].x -=speed;
+        } else
+        if(hands[i].type == 'Rock' &&hands[id].type == 'Scissors' && hands[i].x - hands[id].y < 25){
+            hands[i].y + speed;
+        } else if(hands[i].type == 'Rock' &&hands[id].type == 'Scissors' && hands[i].x - hands[id].y > 25){
+            hands[i].y -= speed;
+        }
+        else { 
+           hands[i].x += hands[i].vx;
+           hands[i].y += hands[i].vy;
+        }
+        
     }
-    if (this.y + this.diameter / 2 > height) {
-      this.y = height - this.diameter / 2;
-      this.vy *= friction;
-    } else if (this.y - this.diameter / 2 < 0) {
-      this.y = this.diameter / 2;
-      this.vy *= friction;
+      
+
+       //bounce
+        if ( hands[i].x > 400 - diameter || hands[i].x < diameter){
+            hands[i].vx = -hands[i].vx;
+          }
+          if ( hands[i].y > height -  diameter ||  hands[i].y <  diameter){
+            hands[i].vy = -hands[i].vx;
+          }
+          
     }
-   
-  }
-
-  display() {
-    fill(this.col)
-    ellipse(this.x, this.y, this.diameter, this.diameter);
-  }
-
-  doImmuneSystem(){
-    if(second()+1 > this.healWait && this.health == "infected"){
-      this.recover();
-     }
-  }
+    
 }
-
-  
+}
